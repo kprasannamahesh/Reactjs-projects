d3f57de59f84702b9c740e208a40ef24397e8d2f
K Prasanna Mahesh <kprasannamahesh007@gmail.com>
2023-05-01 10:59:16 +0530

    Removed Contact card

--- Contact-Manager/src/components/ContactList.jsx
+++ Contact-Manager/src/components/ContactList.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import ContactCard from './ContactCard';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
